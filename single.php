d417f8f9bdd656593270bb3d1bd09dad6b12f03a
Adrian Abal <aabalalonso@danielcastelao.org>
2016-12-11 15:02:53 +0000

    añadido del archivo single , el cual sirve para hacer los posts y la imagen que me faltaba del error 404

---
+++ single.php
@@ -0,0 +1,13 @@
+
+        <h1>Hola a todo guapiiiiiiiiiiisimos , aquie Vegeta 777</h1>
+        <img src="wp-content/themes/Temitaguapo/salamanca.jpg"></img>
+        <?php if(have_posts()){
+while (have_posts()){
+    the_post();
+        the_content();
+}
+}
+        
+        
+        
+       ?> 
